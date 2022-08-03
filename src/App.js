a19698d7863d985235211d247e30c076db27ff6d
azawert <azawertik@gmail.com>
2022-08-03 18:04:49 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Container from "@mui/material/Container";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
@@ -6,14 +7,18 @@ import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 function App() {
   return (
     <>
-      <Header />
-      <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
-      </Container>
+      <BrowserRouter>
+        <Header />
+        <Container maxWidth="lg">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts:id" element={<FullPost />} />
+            <Route path="/add-post" element={<AddPost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </>
   );
 }
