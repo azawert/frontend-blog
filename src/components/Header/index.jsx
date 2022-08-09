1c7de49174779308e8a6cd1e2bf86270d35a4c2d
Igor <azawertik@gmail.com>
2022-08-10 00:49:31 +0300

    sas

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 
 import styles from "./Header.module.scss";
 import Container from "@mui/material/Container";
-import { selectIsAuth } from "../../redux/slices/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsAuth, logout } from "../../redux/slices/auth";
 
 export const Header = () => {
   const isAuth = useSelector(selectIsAuth);
   const dispatch = useDispatch();
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm("Вы точно хотите выйти из своего аккаунта?")) {
+      dispatch(logout());
+      window.localStorage.removeItem("token");
+    }
+  };
 
   return (
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
+          <Link className={styles.logo} to="/">
             <div>ARCHAKOV BLOG</div>
-          </a>
+          </Link>
           <div className={styles.buttons}>
             {isAuth ? (
               <>
@@ -39,7 +43,7 @@ export const Header = () => {
                 <Link to="/login">
                   <Button variant="outlined">Войти</Button>
                 </Link>
-                <Link to="/register">
+                <Link to="/registration">
                   <Button variant="contained">Создать аккаунт</Button>
                 </Link>
               </>
